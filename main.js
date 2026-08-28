a750d7460a49a46fe09b8cf4e70faf4f73a2fd9d
GabrielFl81 <gabriel.lopes.silva24@escola.pr.gov.br>
2026-08-28 08:25:17 -0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,12 @@
+const numeroSenha = document.querySelector('.parametro-senha__texto');
+
+numeroSenha.textContent = 0;
+
+const botoes = document.querySelectorAll('.parametro-senha__botao');
+
+botoes[0].onclick = diminuiTamanho;
+
+function diminuiTamanho(){
+    
+}
+console.log(botoes);
