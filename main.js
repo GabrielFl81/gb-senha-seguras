72d855050080ef3d760d4c90fd9fcade9a2c8174
GabrielFl81 <gabriel.lopes.silva24@escola.pr.gov.br>
2026-09-18 08:30:42 -0300

    Add files via upload

--- main.js
+++ main.js
@@ -1,8 +1,15 @@
 const numeroSenha = document.querySelector('.parametro-senha__texto');
 let tamanhoSenha = 8;
 numeroSenha.textContent = tamanhoSenha;
-
+const letrasMaiusculas = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
+const letrasMinusculas = 'abcdefghijklmnopqrstuvwxyz';
+const numeros = '0123456789';
+const simbolos = '!@%*/?';
 const botoes = document.querySelectorAll('.parametro-senha__botao');
+const campoSenha = document.querySelector('#campo-senha');
+const checkbox = document.querySelectorAll('.checkbox');
+const forcaSenha = document.querySelector('.forca');
+
 
 botoes[0].onclick = diminuiTamanho;
 botoes[1].onclick = aumentaTamanho;
@@ -23,18 +30,43 @@ function aumentaTamanho(){
     geraSenha();
 }
 
-const campoSenha = document.querySelector('#campo-senha');
+for (i = 0; i < checkbox.length;i++){
+    checkbox[i].onclick = geraSenha;
+}
 
-const letrasMaiusculas = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
 geraSenha();
 
 function geraSenha(){
-
+    let alfabeto = '';
+    if(checkbox[0].checked){
+        alfabeto = alfabeto + letrasMaiusculas;
+    }
+    if(checkbox[1].checked){
+        alfabeto = alfabeto + letrasMinusculas;
+    }
+    if(checkbox[2].checked){
+        alfabeto = alfabeto + numeros;
+    }
+    if(checkbox[3].checked){
+        alfabeto = alfabeto + simbolos;
+    }
+    let senha = '';
     for(let i = 0; i < tamanhoSenha;i++){
-        let numeroAleatorio = Math.random()*letrasMaiusculas.length;
+        let numeroAleatorio = Math.random()*alfabeto.length;
         numeroAleatorio = Math.floor(numeroAleatorio);
-        console.log(letrasMaiusculas[numeroAleatorio]);
+        senha = senha + alfabeto[numeroAleatorio];
     };
+    campoSenha.value = senha;
+    classificaSenha();
 }
 
-campoSenha.value = letrasMaiusculas;
+function classificaSenha(){
+    forcaSenha.classList.remove('fraca', 'media', 'forte');
+    if(tamanhoSenha > 13){
+        forcaSenha.classList.add('forte');
+    } else if(tamanhoSenha > 6){
+        forcaSenha.classList.add('media');
+    } else {
+        forcaSenha.classList.add('fraca');
+    }
+}
